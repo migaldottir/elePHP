063218bb9d548a8036297f0ce2a094ccde486064
migaldottir <none399@outlook.com>
2023-05-15 17:13:17 +0200

    initial commit

---
+++ home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> XAMPP (2021)</h1>
+    <p></p>
+    <?php
+    echo "<p>Hello World from PHP... </p>\n";
+    echo "here it is " . date('j-m-y h-i-s') . " yes";
+    ?>
+</body>
+</html>
